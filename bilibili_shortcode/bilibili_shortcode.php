dfa169cb5797ac19f2a89b20277662c1e55ac9aa
God-2077 <115924285+God-2077@users.noreply.github.com>
2023-10-29 09:00:24 +0800

    Update bilibili_shortcode.php

--- bilibili_shortcode/bilibili_shortcode.php
+++ bilibili_shortcode/bilibili_shortcode.php
@@ -3,44 +3,46 @@
 Plugin Name: WordPress通过短代码插入bilibili视频
 Plugin URI: https://github.com/God-2077/bilibili_shortcode/
 Description: 一个WordPress插件，用于使用短代码插入哔哩哔哩视频 & A WordPress plugin for inserting Bilibili videos using shortcodes.
-Version: 1.1
-Author: space520
-Author URI: https://space520eu.org/
+Version: 2.0
+Author: Space520
+Author URI: https://space520.eu.org/
 License: GPL3
 */
 
 // 添加插入bilibili视频短代码
 function vbilibili_shortcode( $atts, $content = null ) {
-   // 默认参数值
-   $defaults = array(
-       'av' => '',
-       'bv' => '',
-   );
 
    // 解析短代码中的参数
    $atts = shortcode_atts( $defaults, $atts );
+  
+   // 提取$content中的所有bv号和av号
+    preg_match_all('/BV([a-zA-Z0-9]+)/', $content, $bv_matches);
+    preg_match_all('/av([0-9]+)/', $content, $av_matches);
    
-   // 如果参数 av 和 bv 都为空，则返回错误信息
-   if ( empty( $atts['av'] ) && empty( $atts['bv'] ) ) {
-       return '<p>av 或 bv 不可为空</p>';
+   // 构建iframe的HTML代码
+   $iframes = '';
+   
+   // 生成bv号对应的iframe
+   foreach ($bv_matches[1] as $bv) {
+        
+        
+       $iframe_url = 'https://player.bilibili.com/player.html?bvid=' . $bv . '&amp;high_quality=1';
+       $iframe = '<div style="position: relative; padding: 30% 45%;"><iframe src="' . $iframe_url . '" frameborder="no" scrolling="no" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts" allowfullscreen="allowfullscreen" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> </iframe></div><br>';
+       $iframes .= $iframe;
+   }
+   
+   // 生成av号对应的iframe
+   foreach ($av_matches[1] as $av) {
+   
+        // 将$av转换为整数类型
+        $av = intval($av);
+   
+       $iframe_url = 'https://player.bilibili.com/player.html?aid=' . $av . '&amp;high_quality=1';
+       $iframe = '<div style="position: relative; padding: 30% 45%;"><iframe src="' . $iframe_url . '" frameborder="no" scrolling="no" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts" allowfullscreen="allowfullscreen" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> </iframe></div><br>';
+       $iframes .= $iframe;
    }
 
-   // 如果参数 av 和 bv 同时存在，则返回错误信息
-   if ( ! empty( $atts['av'] ) && ! empty( $atts['bv'] ) ) {
-       return '<p>av 和 bv 不可同时存在</p>';
-   }
-
-   // 根据参数 av 或 bv 构建 iframe 的 URL
-   if ( ! empty( $atts['av'] ) ) {
-       $iframe_url = 'https://player.bilibili.com/player.html?aid=' . esc_attr( $atts['av'] ) . '&amp;high_quality=1';
-   } elseif ( ! empty( $atts['bv'] ) ) {
-       $iframe_url = 'https://player.bilibili.com/player.html?bvid=' . esc_attr( $atts['bv'] ) . '&amp;high_quality=1';
-   }
-
-   // 构建 iframe 的 HTML 代码
-   $iframe = '<div style="position: relative; padding: 30% 45%;"><iframe src="' . $iframe_url . '" frameborder="no" scrolling="no" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts" allowfullscreen="allowfullscreen" style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;"> </iframe></div><br>';
-
-   // 返回 iframe 的 HTML 代码
-   return $iframe;
+   // 返回所有iframe的HTML代码
+   return $iframes;
 }
 add_shortcode( 'vbilibili', 'vbilibili_shortcode' );
